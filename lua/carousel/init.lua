6f48d8b82b5ec5372ce00b33d530884fe01bcb17
David Woodland <65493742+davidrwoodland@users.noreply.github.com>
2023-10-25 20:18:07 +1100

    fingers crossed working copy

--- lua/carousel/init.lua
+++ lua/carousel/init.lua
@@ -1,11 +1,8 @@
 local around = require("carousel.carousel")
 local config = require("carousel.config")
 
-local carouselKeymapForward = '<M-p>'
-local lesuoracKeymapBackward = '<M-P>'
-
-local carouselKeymapForward = config.customKeymapForward or carouselKeymapForward
-local lesuoracKeymapBackward = config.customKeymapBackward or lesuoracKeymapBackward
+local carouselKeymapForward = config.customKeymapForward or '<M-p>'
+local lesuoracKeymapBackward = config.customKeymapBackward or '<M-P>'
 
 vim.keymap.set("n", carouselKeymapForward, function() around.carousel() end)
 vim.keymap.set("n", lesuoracKeymapBackward, function() around.lesuorac() end)
